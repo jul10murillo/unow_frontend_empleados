f5a0071d26aa551accba643c0a6e28d56a0ed0bf
Julio Murillo <julioamurillo10@gmail.com>
2025-02-17 11:26:15 -0500

    Feat: Login and register, Api axios

---
+++ src/components/Login.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import api from '../services/api';
+import '../App.css'; // Asegúrate de importar el archivo CSS
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await api.post('/login_check', { email, password });
+      localStorage.setItem('token', response.data.token);
+      navigate('/employees/list');
+    } catch (error) {
+      console.error('Error logging in:', error);
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <h1 className="login-title">Prueba técnica Unow</h1>
+      <form onSubmit={handleSubmit} className="login-form">
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+          required
+          className="login-input"
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+          required
+          className="login-input"
+        />
+        <button type="submit" className="login-button">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
